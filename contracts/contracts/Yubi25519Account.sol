4004bdd06741a2ca4b59aed558598e65da775410
tommymsz006 <37748412+tommymsz006@users.noreply.github.com>
2023-05-13 02:14:34 +0100

    add prove algo of SHA-256 and Ed25519

--- contracts/contracts/Yubi25519Account.sol
+++ contracts/contracts/Yubi25519Account.sol
@@ -6,20 +6,21 @@ pragma solidity ^0.8.18;
 /* solhint-disable reason-string */
 
 import "./SimpleAccount.sol";
+import "./libraries/Ed25519.sol";
 
-/**
-  * minimal account.
-  *  this is sample minimal account.
-  *  has execute, eth handling methods
-  *  has a single signer that can send requests through the entryPoint.
-  */
 contract Yubi25519Account is SimpleAccount {
+
+    constructor(IEntryPoint anEntryPoint) SimpleAccount(anEntryPoint) {
+    }
+
     /// implement template method of BaseAccount
     function _validateSignature(UserOperation calldata userOp, bytes32 userOpHash)
     internal override virtual returns (uint256 validationData) {
-        bytes32 hash = userOpHash.toEthSignedMessageHash();
-        if (owner != hash.recover(userOp.signature))
+        ( bytes32 k, bytes32 r, bytes32 s, bytes memory m ) = abi.decode(userOp.signature, (bytes32, bytes32, bytes32, bytes));
+
+        if (Ed25519.verify(k, r, s, m))
             return SIG_VALIDATION_FAILED;
+
         return 0;
     }
 }
